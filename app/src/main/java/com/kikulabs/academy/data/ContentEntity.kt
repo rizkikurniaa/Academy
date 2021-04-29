597694f0e10beb56fc01d5c53d4801b599d1005a
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-04-29 21:07:46 +0700

    Initial commit

---
+++ app/src/main/java/com/kikulabs/academy/data/ContentEntity.kt
@@ -0,0 +1,5 @@
+package com.kikulabs.academy.data
+
+data class ContentEntity(
+    var content: String?
+)
---
+++ app/src/main/java/com/kikulabs/academy/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.kikulabs.academy.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kikulabs.academy.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
