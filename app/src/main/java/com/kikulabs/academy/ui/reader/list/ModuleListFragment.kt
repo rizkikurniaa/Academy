b2423230024ae70e8987da82b276b95277d31b1d
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-05-01 10:21:34 +0700

    Implemented viewmodel

--- app/src/main/java/com/kikulabs/academy/ui/reader/list/ModuleListFragment.kt
+++ app/src/main/java/com/kikulabs/academy/ui/reader/list/ModuleListFragment.kt
@@ -2,18 +2,18 @@ package com.kikulabs.academy.ui.reader.list
 
 import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.kikulabs.academy.R
 import com.kikulabs.academy.data.ModuleEntity
 import com.kikulabs.academy.databinding.FragmentModuleListBinding
 import com.kikulabs.academy.ui.reader.CourseReaderActivity
 import com.kikulabs.academy.ui.reader.CourseReaderCallback
-import com.kikulabs.academy.utils.DataDummy
+import com.kikulabs.academy.ui.reader.CourseReaderViewModel
 
 class ModuleListFragment : Fragment(), MyAdapterClickListener {
 
@@ -26,6 +26,7 @@ class ModuleListFragment : Fragment(), MyAdapterClickListener {
     private lateinit var fragmentModuleListBinding: FragmentModuleListBinding
     private lateinit var adapter: ModuleListAdapter
     private lateinit var courseReaderCallback: CourseReaderCallback
+    private lateinit var viewModel: CourseReaderViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,8 +40,11 @@ class ModuleListFragment : Fragment(), MyAdapterClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(requireActivity(), ViewModelProvider.NewInstanceFactory())[CourseReaderViewModel::class.java]
         adapter = ModuleListAdapter(this)
-        populateRecyclerView(DataDummy.generateDummyModules("a14"))
+        populateRecyclerView(viewModel.getModules())
+
     }
 
     override fun onAttach(context: Context) {
@@ -50,6 +54,7 @@ class ModuleListFragment : Fragment(), MyAdapterClickListener {
 
     override fun onItemClicked(position: Int, moduleId: String) {
         courseReaderCallback.moveTo(position, moduleId)
+        viewModel.setSelectedModule(moduleId)
     }
 
     private fun populateRecyclerView(modules: List<ModuleEntity>) {
