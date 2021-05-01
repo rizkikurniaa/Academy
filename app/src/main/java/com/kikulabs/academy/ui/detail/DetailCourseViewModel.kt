57f8abdb7c4197912ecad20f256e4f5c91ab05f2
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-05-01 10:18:58 +0700

    Created detail course viewmodel

---
+++ app/src/main/java/com/kikulabs/academy/ui/detail/DetailCourseViewModel.kt
@@ -0,0 +1,27 @@
+package com.kikulabs.academy.ui.detail
+
+import androidx.lifecycle.ViewModel
+import com.kikulabs.academy.data.CourseEntity
+import com.kikulabs.academy.data.ModuleEntity
+import com.kikulabs.academy.utils.DataDummy
+
+class DetailCourseViewModel : ViewModel() {
+    private lateinit var courseId: String
+
+    fun setSelectedCourse(courseId: String) {
+        this.courseId = courseId
+    }
+
+    fun getCourse(): CourseEntity {
+        lateinit var course: CourseEntity
+        val coursesEntities = DataDummy.generateDummyCourses()
+        for (courseEntity in coursesEntities) {
+            if (courseEntity.courseId == courseId) {
+                course = courseEntity
+            }
+        }
+        return course
+    }
+
+    fun getModules(): List<ModuleEntity> = DataDummy.generateDummyModules(courseId)
+}
