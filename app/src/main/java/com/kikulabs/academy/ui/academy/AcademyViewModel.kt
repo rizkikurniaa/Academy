150da8e87f08c932a99f792f6b321c23bb5f7df3
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-05-07 22:55:48 +0700

    Added constructor and access AcademyRepository

--- app/src/main/java/com/kikulabs/academy/ui/academy/AcademyViewModel.kt
+++ app/src/main/java/com/kikulabs/academy/ui/academy/AcademyViewModel.kt
@@ -2,8 +2,8 @@ package com.kikulabs.academy.ui.academy
 
 import androidx.lifecycle.ViewModel
 import com.kikulabs.academy.data.CourseEntity
-import com.kikulabs.academy.utils.DataDummy
+import com.kikulabs.academy.data.source.AcademyRepository
 
-class AcademyViewModel : ViewModel() {
-    fun getCourses(): List<CourseEntity> = DataDummy.generateDummyCourses()
+class AcademyViewModel(private val academyRepository: AcademyRepository) : ViewModel() {
+    fun getCourses(): List<CourseEntity> = academyRepository.getAllCourses()
 }
--- app/src/main/java/com/kikulabs/academy/ui/detail/DetailCourseViewModel.kt
+++ app/src/main/java/com/kikulabs/academy/ui/detail/DetailCourseViewModel.kt
@@ -3,25 +3,16 @@ package com.kikulabs.academy.ui.detail
 import androidx.lifecycle.ViewModel
 import com.kikulabs.academy.data.CourseEntity
 import com.kikulabs.academy.data.ModuleEntity
-import com.kikulabs.academy.utils.DataDummy
+import com.kikulabs.academy.data.source.AcademyRepository
 
-class DetailCourseViewModel : ViewModel() {
+class DetailCourseViewModel(private val academyRepository: AcademyRepository) : ViewModel() {
     private lateinit var courseId: String
 
     fun setSelectedCourse(courseId: String) {
         this.courseId = courseId
     }
 
-    fun getCourse(): CourseEntity {
-        lateinit var course: CourseEntity
-        val coursesEntities = DataDummy.generateDummyCourses()
-        for (courseEntity in coursesEntities) {
-            if (courseEntity.courseId == courseId) {
-                course = courseEntity
-            }
-        }
-        return course
-    }
+    fun getCourse(): CourseEntity = academyRepository.getCourseWithModules(courseId)
 
-    fun getModules(): List<ModuleEntity> = DataDummy.generateDummyModules(courseId)
+    fun getModules(): List<ModuleEntity> = academyRepository.getAllModulesByCourse(courseId)
 }
