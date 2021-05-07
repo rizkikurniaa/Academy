941b373f9defebefcf8f2c88c4fda067d7d87706
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-05-07 22:57:27 +0700

    Created ViewModelFactory

---
+++ app/src/main/java/com/kikulabs/academy/viewmodel/ViewModelFactory.kt
@@ -0,0 +1,46 @@
+package com.kikulabs.academy.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.kikulabs.academy.data.source.AcademyRepository
+import com.kikulabs.academy.di.Injection
+import com.kikulabs.academy.ui.academy.AcademyViewModel
+import com.kikulabs.academy.ui.bookmark.BookmarkViewModel
+import com.kikulabs.academy.ui.detail.DetailCourseViewModel
+import com.kikulabs.academy.ui.reader.CourseReaderViewModel
+
+class ViewModelFactory private constructor(private val mAcademyRepository: AcademyRepository) : ViewModelProvider.NewInstanceFactory() {
+
+    companion object {
+        @Volatile
+        private var instance: ViewModelFactory? = null
+
+        fun getInstance(context: Context): ViewModelFactory =
+            instance ?: synchronized(this) {
+                instance ?: ViewModelFactory(Injection.provideRepository(context)).apply {
+                    instance = this
+                }
+            }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        when {
+            modelClass.isAssignableFrom(AcademyViewModel::class.java) -> {
+                return AcademyViewModel(mAcademyRepository) as T
+            }
+            modelClass.isAssignableFrom(DetailCourseViewModel::class.java) -> {
+                return DetailCourseViewModel(mAcademyRepository) as T
+            }
+            modelClass.isAssignableFrom(BookmarkViewModel::class.java) -> {
+                return BookmarkViewModel(mAcademyRepository) as T
+            }
+            modelClass.isAssignableFrom(CourseReaderViewModel::class.java) -> {
+                return CourseReaderViewModel(mAcademyRepository) as T
+            }
+            else -> throw Throwable("Unknown ViewModel class: " + modelClass.name)
+        }
+
+    }
+}
