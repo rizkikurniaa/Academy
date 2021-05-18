445df58835f72c9811db96c6d7595780e659431f
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-05-19 00:57:33 +0700

    Added handler

--- app/src/main/java/com/kikulabs/academy/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/kikulabs/academy/data/source/remote/RemoteDataSource.kt
@@ -1,5 +1,7 @@
 package com.kikulabs.academy.data.source.remote
 
+import android.os.Handler
+import android.os.Looper
 import com.kikulabs.academy.data.source.remote.response.ContentResponse
 import com.kikulabs.academy.data.source.remote.response.CourseResponse
 import com.kikulabs.academy.data.source.remote.response.ModuleResponse
@@ -7,7 +9,12 @@ import com.kikulabs.academy.utils.JsonHelper
 
 class RemoteDataSource private constructor(private val jsonHelper: JsonHelper) {
 
+    private val handler = Handler(Looper.getMainLooper())
+
     companion object {
+
+        private const val SERVICE_LATENCY_IN_MILLIS: Long = 2000
+
         @Volatile
         private var instance: RemoteDataSource? = null
 
@@ -17,11 +24,38 @@ class RemoteDataSource private constructor(private val jsonHelper: JsonHelper) {
             }
     }
 
-    fun getAllCourses(): List<CourseResponse> = jsonHelper.loadCourses()
+    fun getAllCourses(callback: LoadCoursesCallback) {
+        handler.postDelayed(
+            { callback.onAllCoursesReceived(jsonHelper.loadCourses()) },
+            SERVICE_LATENCY_IN_MILLIS
+        )
+    }
 
-    fun getModules(courseId: String): List<ModuleResponse> = jsonHelper.loadModule(courseId)
+    fun getModules(courseId: String, callback: LoadModulesCallback) {
+        handler.postDelayed(
+            { callback.onAllModulesReceived(jsonHelper.loadModule(courseId)) },
+            SERVICE_LATENCY_IN_MILLIS
+        )
+    }
 
-    fun getContent(moduleId: String): ContentResponse = jsonHelper.loadContent(moduleId)
+    fun getContent(moduleId: String, callback: LoadContentCallback) {
+        handler.postDelayed(
+            { callback.onContentReceived(jsonHelper.loadContent(moduleId)) },
+            SERVICE_LATENCY_IN_MILLIS
+        )
+    }
+
+    interface LoadCoursesCallback {
+        fun onAllCoursesReceived(courseResponses: List<CourseResponse>)
+    }
+
+    interface LoadModulesCallback {
+        fun onAllModulesReceived(moduleResponses: List<ModuleResponse>)
+    }
+
+    interface LoadContentCallback {
+        fun onContentReceived(contentResponse: ContentResponse)
+    }
 
 
 }
