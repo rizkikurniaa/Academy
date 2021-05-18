cee119deb2109d978719a2a05c37081e5c11a0b9
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-05-19 00:58:20 +0700

    Changed return value to LiveData

--- app/src/main/java/com/kikulabs/academy/data/AcademyDataSource.kt
+++ app/src/main/java/com/kikulabs/academy/data/AcademyDataSource.kt
@@ -1,17 +1,18 @@
 package com.kikulabs.academy.data
 
+import androidx.lifecycle.LiveData
 import com.kikulabs.academy.data.source.local.entity.CourseEntity
 import com.kikulabs.academy.data.source.local.entity.ModuleEntity
 
 interface AcademyDataSource {
 
-    fun getAllCourses(): List<CourseEntity>
+    fun getAllCourses(): LiveData<List<CourseEntity>>
 
-    fun getBookmarkedCourses(): List<CourseEntity>
+    fun getBookmarkedCourses(): LiveData<List<CourseEntity>>
 
-    fun getCourseWithModules(courseId: String): CourseEntity
+    fun getCourseWithModules(courseId: String): LiveData<CourseEntity>
 
-    fun getAllModulesByCourse(courseId: String): List<ModuleEntity>
+    fun getAllModulesByCourse(courseId: String): LiveData<List<ModuleEntity>>
 
-    fun getContent(courseId: String, moduleId: String): ModuleEntity
+    fun getContent(courseId: String, moduleId: String): LiveData<ModuleEntity>
 }
