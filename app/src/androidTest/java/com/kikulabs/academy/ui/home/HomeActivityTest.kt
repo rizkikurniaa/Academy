fad7eb9e122ee691acd8d72c8ff37dfa62841264
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-05-25 23:58:20 +0700

    Done Instrumental testing

--- app/src/androidTest/java/com/kikulabs/academy/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/com/kikulabs/academy/ui/home/HomeActivityTest.kt
@@ -21,6 +21,7 @@ class HomeActivityTest {
 
     @Test
     fun loadCourses() {
+        delayThreeSecond()
         onView(withId(R.id.rv_academy)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_academy)).perform(
             RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
@@ -31,12 +32,14 @@ class HomeActivityTest {
 
     @Test
     fun loadDetailCourse() {
+        delayThreeSecond()
         onView(withId(R.id.rv_academy)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
                 0,
                 click()
             )
         )
+        delayThreeSecond()
         onView(withId(R.id.text_title)).check(matches(isDisplayed()))
         onView(withId(R.id.text_title)).check(matches(withText(dummyCourse[0].title)))
         onView(withId(R.id.text_date)).check(matches(isDisplayed()))
@@ -45,43 +48,59 @@ class HomeActivityTest {
 
     @Test
     fun loadModule() {
+        delayThreeSecond()
         onView(withId(R.id.rv_academy)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
                 0,
                 click()
             )
         )
+        delayThreeSecond()
         onView(withId(R.id.btn_start)).perform(click())
+        delayThreeSecond()
         onView(withId(R.id.rv_module)).check(matches(isDisplayed()))
     }
 
     @Test
     fun loadDetailModule() {
+        delayThreeSecond()
         onView(withId(R.id.rv_academy)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
                 0,
                 click()
             )
         )
+        delayThreeSecond()
         onView(withId(R.id.btn_start)).perform(click())
+        delayThreeSecond()
         onView(withId(R.id.rv_module)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
                 0,
                 click()
             )
         )
+        delayThreeSecond()
         onView(withId(R.id.web_view)).check(matches(isDisplayed()))
     }
 
     @Test
     fun loadBookmarks() {
         onView(withText("Bookmark")).perform(click())
+        delayThreeSecond()
         onView(withId(R.id.rv_bookmark)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_bookmark)).perform(
             RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
                 dummyCourse.size
             )
         )
+    }
+
+    private fun delayThreeSecond() {
+        try {
+            Thread.sleep(3000)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
     }
 
 }
