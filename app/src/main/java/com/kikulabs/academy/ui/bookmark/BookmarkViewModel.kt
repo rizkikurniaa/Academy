3bb89a5e09655ba1dc43d4db427c44659527686e
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-06-12 12:52:07 +0700

    Implemented swipe

--- app/src/main/java/com/kikulabs/academy/ui/bookmark/BookmarkViewModel.kt
+++ app/src/main/java/com/kikulabs/academy/ui/bookmark/BookmarkViewModel.kt
@@ -8,4 +8,9 @@ import com.kikulabs.academy.data.AcademyRepository
 
 class BookmarkViewModel(private val academyRepository: AcademyRepository) : ViewModel() {
     fun getBookmarks(): LiveData<PagedList<CourseEntity>> = academyRepository.getBookmarkedCourses()
+
+    fun setBookmark(courseEntity: CourseEntity) {
+        val newState = !courseEntity.bookmarked
+        academyRepository.setCourseBookmark(courseEntity, newState)
+    }
 }
